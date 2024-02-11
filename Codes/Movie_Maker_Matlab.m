0b1daca4d8ac9c72e94c1a5f7263cd8d5de12be9
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2024-02-11 15:40:00 +0100

    Add files via upload

--- Codes/Movie_Maker_Matlab.m
+++ Codes/Movie_Maker_Matlab.m
@@ -1,4 +1,6 @@
-﻿clc
+%https://doi.org/10.1016/j.enconman.2008.09.003
+%https://github.com/Raphael-Boichot/Tree-network-structure-generation-for-heat-conduction-by-cellular-automaton
+clc
 clear
 
 disp('------------------------------------------')
@@ -9,7 +11,7 @@ vidfile = VideoWriter('Output.mp4','MPEG-4');
 open(vidfile);
 listing = dir('*.png');
 for i=1:1:length(listing)
-    name=listing(i).name
+    name=listing(i).name;
     frame=imread(name);
     [height, width, null]=size(frame);
     frame=imresize(frame,800/height,'nearest');
